ce822e52cfbb0b6128c1294ba6cb6c11c6025281
Vishwanath M L <vishwanathml.isds@gmail.com>
2017-02-08 15:12:53 +0530

    Add files via upload

---
+++ OddNumWords.java
@@ -0,0 +1,54 @@
+public class OddNumWords {
+
+	/**
+	 * This Java program is to print only odd repated words.
+	 * 
+	 */
+	public static void main(String[] args) {
+		//this is the string used to find is leter is;
+		String[] tstStringArray = {"abcdddd","abcdddd","abc","abc","abc","de","abc","de","abc","de","de"}; //The in put words
+		String[] myStringArray = new String[tstStringArray.length]; //Array to store only once if the word is repeated.
+		//String str = new String();
+		int chrCount = 0,chrPos =-1,insPos=0;
+		int[] myIntArray = new int[tstStringArray.length];
+		// TODO Auto-generated method stub
+		for (int i=0; i< tstStringArray.length;i++)
+		{
+			//str=tstStringArray[i];
+			//System.out.println(i+":"+str);
+			chrCount=0;
+			chrPos =-1;
+			for (int j=0;j<myStringArray.length;j++)
+			{
+				if (tstStringArray[i] == myStringArray[j])
+				{
+					chrCount=1;
+					chrPos=j;
+					break;
+				}
+			}
+			
+			if (chrCount == 1)
+			{
+				myIntArray[chrPos] = myIntArray[chrPos]+1;
+			}
+			else
+			{
+				myStringArray[insPos] = tstStringArray[i];
+				myIntArray[insPos] = 1;
+				insPos=insPos+1;
+			}
+			
+		}
+		for (int k =0; k<myStringArray.length;k++)
+		{
+			if ((myIntArray[k]%2) != 0)
+			{
+				System.out.println(myStringArray[k]+" -->" +myIntArray[k]);
+			}
+		}
+		System.out.println("Bye");
+
+	}
+
+}
